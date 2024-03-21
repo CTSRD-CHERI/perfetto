24649f6d4411c2bb3d8ca7e29f743c6d4786d023
Graeme Jenkinson <graeme@capabilitieslimited.co.uk>
2024-03-21 17:18:10 +0000

    [cheriabi Ammend TraceBlob to prevent padding
    As capabilities have strong alignment requirements padding is inserted
    into the `TraceBlob` class between the size_t and pointer width types
    invalidating the static assert.

--- include/perfetto/trace_processor/trace_blob.h
+++ include/perfetto/trace_processor/trace_blob.h
@@ -77,9 +77,15 @@ class PERFETTO_EXPORT_COMPONENT TraceBlob : public RefCounted {
   TraceBlob(Ownership ownership, uint8_t* data, size_t size);
 
   Ownership ownership_ = Ownership::kNullOrMmapped;
+#if defined(__CHERI_PURE_CAPABILITY__)
+  uint8_t* data_ = nullptr;
+  std::unique_ptr<base::ScopedMmap> mapping_;
+  size_t size_ = 0;
+#else
   uint8_t* data_ = nullptr;
   size_t size_ = 0;
   std::unique_ptr<base::ScopedMmap> mapping_;
+#endif
 };
 
 }  // namespace trace_processor
